58bc55568261e23ecb75f88bff5ca733be3c7e21
JMRomero <rjosemiguel787@gmail.com>
2024-10-22 11:31:52 -0500

    tabla empresas scriptSQL

--- ScriptSql.sql
+++ ScriptSql.sql
@@ -1,3 +1,4 @@
 create database PruebaConocimiento;
 
 create table usuarios(id int not null,u_name Nvarchar(150),u_last_name nvarchar(150));
+create table empresa(id int nult null,e_name nvarchar(150),e_nit float);
