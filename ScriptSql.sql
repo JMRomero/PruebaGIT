3d31b731299c6694b16e1d82d138c1d4ee0eec2a
JMRomero <rjosemiguel787@gmail.com>
2024-10-22 12:11:51 -0500

    cambios insercion sql

--- ScriptSql.sql
+++ ScriptSql.sql
@@ -2,4 +2,4 @@ create database PruebaConocimiento;
 
 create table usuarios(id int not null,u_name Nvarchar(150),u_last_name nvarchar(150));
 create table empresa(id int nult null,e_name nvarchar(150),e_nit float);
-insert into usuarios values(1,'j','r'),(2,'j','s');
+insert into usuarios values(1,'j','r'),(2,'j','s'),(3,'m','c');
