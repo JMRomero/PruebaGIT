3cf3f7904869951191dbe9898ff7c175fa0b70d2
JMRomero <rjosemiguel787@gmail.com>
2024-10-23 07:50:27 -0500

    cambio desde ramaJS en busca de conflictos

--- ScriptSql.sql
+++ ScriptSql.sql
@@ -1,8 +1,8 @@
 create database PruebaConocimiento;
 
-create table usuarios(id int not null,u_name Nvarchar(150),u_last_name nvarchar(150));
-create table empresa(id int nult null,e_name nvarchar(150),e_nit float);
+create table usuarios(id int not null,u_name Nvarchar(150));
+create table empresa(id int nult null,e_name nvarchar(150),e_nit float,N_contacto int not null);
 
-insert into usuarios values(4,'j','r'),(2,'j','s'),(3,'m','c'),(4,'m','m'),(5,'n','e'),(6,'r','r');
+insert into usuarios values(4,'j','r'),(2,'j','s'),(3,'m','c'),(4,'m','m'),(5,'n','e'),(6,'ramaJS','ramaJS');
 --comentario t-SQL
 --otro comentario
