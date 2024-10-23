2347a560ab6f6c23e6b522be317399653cfcf2ce
JMRomero <rjosemiguel787@gmail.com>
2024-10-23 07:19:43 -0500

    segunda prueba de merge en busca de conflictos

--- ScriptSql.sql
+++ ScriptSql.sql
@@ -1,7 +1,2 @@
-create database PruebaConocimiento;
-
-create table usuarios(id int not null,u_name Nvarchar(150),u_last_name nvarchar(150));
-create table empresa(id int nult null,e_name nvarchar(150),e_nit float);
-insert into usuarios values(4,'j','r'),(2,'j','s'),(3,'m','c'),(4,'m','m');
---comentario t-SQL
+--comentario desde ramaJS
 
